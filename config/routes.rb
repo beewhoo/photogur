9887766761b2e685e1e57ad8e7ccea9ebedcf8b0
beewhoo <bibeksinghthapa@gmail.com>
2018-06-12 17:21:15 -0400

    did whole bunch of stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'pictures' => 'pictures#index'
+
+  root   'pictures#index'
+
+  get    'pictures'         => "pictures#index"
+
+  post   'pictures'         => "pictures#create"
+  get    'pictures/new'     => "pictures#new"
+
+  get    'pictures/:id/edit'=> "pictures#edit"
+  patch  'pictures/:id'     => "pictures#update"
+
+
+  get    'pictures/:id'     => "pictures#show"
+
+  delete 'pictures/:id'     => "pictures#destroy"
+
 
 end
